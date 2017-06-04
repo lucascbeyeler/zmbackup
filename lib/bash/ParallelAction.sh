34346fb21b2521488a07d6d04f563e4927cd4554
Lucas Costa Beyeler <lucas.costab@outlook.com>
2017-06-04 20:37:39 -0300

    Bug fix found during the tests

--- lib/bash/ParallelAction.sh
+++ lib/bash/ParallelAction.sh
@@ -112,7 +112,8 @@ function mailbox_restore()
 function ldap_filter()
 {
   EXIST=$(grep $1 $WORKDIR/sessions.txt 2> /dev/null | tail -1 | awk -F: '{print $3}')
-  if [[ grep -Fxq $1 /etc/zmbackup/blacklist.conf ]]; then
+  grep -Fxq $1 /etc/zmbackup/blacklist.conf
+  if [[ $? -ne 0 ]]; then
     echo "WARN: $1 found inside blacklist - Nothing to do."
   elif [[ "$EXIST" = "$(date +%m/%d/%y)" && "$LOCK_BACKUP" == "TRUE" ]]; then
     echo "WARN: $1 already has backup today. Nothing to do."
