b1bbe790cba229f4631a03dce8ed8193f9933356
Lucas Costa Beyeler <lucas.costab@outlook.com>
2017-06-04 21:02:42 -0300

    Bug fix found during the tests

--- lib/bash/BackupAction.sh
+++ lib/bash/BackupAction.sh
@@ -80,12 +80,15 @@ function backup_main()
     logger -i --id=$$ -p local7.info "Zmbackup: Backup session $SESSION started on $(date)"
     echo "SESSION: $SESSION started on $(date)" >> $TEMPSESSION
     if [ $SESSION == "full*" ] || [ $SESSION == "inc*" ]; then
+      export -f __backupFullInc
       cat $TEMPACCOUNT | parallel --no-notice --env $2 --jobs $MAX_PARALLEL_PROCESS \
                          '__backupFullInc {} $2'
     elif [ $SESSION == "mbox*" ]; then
+      export -f __backupMailbox
       cat $TEMPACCOUNT | parallel --no-notice --env $2 --jobs $MAX_PARALLEL_PROCESS \
                          '__backupMailbox {} "$2"'
     else
+      export -f __backupLdap
       cat $TEMPACCOUNT | parallel --no-notice --env $2 --jobs $MAX_PARALLEL_PROCESS \
                          '__backupLdap {} "$2"'
     fi
