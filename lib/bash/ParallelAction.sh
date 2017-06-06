b5cab03a3532933d73cfd7c925eada8ec762be1c
Lucas Costa Beyeler <lucas.costab@outlook.com>
2017-06-06 09:02:12 -0300

    Bugfix - Mailbox wasn't backing up

--- lib/bash/ParallelAction.sh
+++ lib/bash/ParallelAction.sh
@@ -38,10 +38,10 @@ function mailbox_backup()
   if [ "$2" == "INC" ]; then
     AFTER="\&query=after:"$(grep $1 $WORKDIR/sessions.txt | tail -1 | awk -F: '{print $3}')
   fi
-  ERR=$((wget --quiet -O $TEMPDIR/$1.tgz --http-user $ADMINUSER --http-passwd $ADMINPASS \
+  ERR=$((wget --quiet -O $TEMPDIR/$1.tgz --user $ADMINUSER --password $ADMINPASS \
         "https://$MAILHOST:7071/home/$1/?fmt=tgz"$AFTER --no-check-certificate) 2>&1)
   if [[ $? -eq 0 ]]; then
-    if [[ -z $TEMPDIR/$1.tgz ]]; then
+    if [[ -s $TEMPDIR/$1.tgz ]]; then
       echo $SESSION:$1:$(date +%m/%d/%y) >> $TEMPSESSION
       logger -i --id=$$ -p local7.info "Zmbackup: Mailbox - Backup for account $1 finished."
     else
