a366743e3495bfd906dcb6d3af76b7d494460d12
Lucas Costa Beyeler <lucas.costab@outlook.com>
2017-06-15 18:21:34 -0300

    Fix for backup option

--- lib/bash/ParallelAction.sh
+++ lib/bash/ParallelAction.sh
@@ -35,13 +35,12 @@ function ldap_backup()
 function mailbox_backup()
 {
   if [[ "$INC" == "TRUE" ]]; then
-    AFTER="\&query=after:"$(grep $1 $WORKDIR/sessions.txt | tail -1 | awk -F: '{print $3}')
+    AFTER="&query=after:"$(grep $1 $WORKDIR/sessions.txt | tail -1 | awk -F: '{print $1}' | cut -d'-' -f1)
   fi
   ERR=$((wget -O $TEMPDIR/$1.tgz --user $ADMINUSER --password $ADMINPASS \
         "https://$MAILHOST:7071/home/$1/?fmt=tgz"$AFTER --no-check-certificate) 2>&1)
   if [[ $? -eq 0 ]]; then
     if [[ -s $TEMPDIR/$1.tgz ]]; then
-      echo $SESSION:$1:$(date +%m/%d/%y) >> $TEMPSESSION
       logger -i -p local7.info "Zmbackup: Mailbox - Backup for account $1 finished."
     else
       logger -i -p local7.info "Zmbackup: Mailbox - Backup for account $1 finished, but the file is empty. Removing..."
