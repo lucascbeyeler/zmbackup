5f3ea9f6e881589fa301c1d1ccea29236618129d
lucascbeyeler <lucas.costab@outlook.com>
2017-09-02 21:59:18 -0300

    Discarting the log sended by SQLite3 and saving the size of the backup inside database

--- project/lib/bash/BackupAction.sh
+++ project/lib/bash/BackupAction.sh
@@ -19,7 +19,7 @@ function __backupFullInc(){
         echo $SESSION:$1:$(date +%m/%d/%y) >> $TEMPSESSION
       elif [[ $SESSION_TYPE == "SQLITE3" ]]; then
         DATE=$(date +%Y-%m-%dT%H:%M:%S.%N)
-        SIZE=$(du -hs $WORKDIR/$i* | awk {'print $1'})
+        SIZE=$(du -hs $WORKDIR/$i* | grep total | cut -f1)
         echo "insert into backup_account (email,sessionID,account_size) values ('$1','$SESSIONID','$SIZE')" >> $TEMPSQL
       fi
     fi
@@ -42,7 +42,7 @@ function __backupLdap(){
       echo $SESSION:$1:$(date +%m/%d/%y) >> $TEMPSESSION
     elif [[ $SESSION_TYPE == "SQLITE3" ]]; then
       DATE=$(date +%Y-%m-%dT%H:%M:%S.%N)
-      SIZE=$(du -hs $WORKDIR/$i* | awk {'print $1'})
+      SIZE=$(du -hs $WORKDIR/$i* | grep total | cut -f1)
       echo "insert into backup_account (email,sessionID,account_size) values ('$1','$SESSIONID','$SIZE')" >> $TEMPSQL
     fi
   fi
@@ -62,7 +62,7 @@ function __backupMailbox(){
       echo $SESSION:$1:$(date +%m/%d/%y) >> $TEMPSESSION
     elif [[ $SESSION_TYPE == "SQLITE3" ]]; then
       DATE=$(date +%Y-%m-%dT%H:%M:%S.%N)
-      SIZE=$(du -hs $WORKDIR/$i* | awk {'print $1'})
+      SIZE=$(du -hs $WORKDIR/$i* | grep total | cut -f1)
       echo "insert into backup_account (email,sessionID,account_size) values ('$1','$SESSIONID','$SIZE')"  >> $TEMPSQL
     fi
   fi
@@ -101,7 +101,7 @@ function backup_main()
       echo "SESSION: $SESSION started on $(date)" >> $TEMPSESSION
     elif [[ $SESSION_TYPE == "SQLITE3" ]]; then
       DATE=$(date +%Y-%m-%dT%H:%M:%S.%N)
-      sqlite3 $WORKDIR/sessions.sqlite3 "insert into backup_session(sessionID,initial_date,type,status) values ('$SESSION','$DATE','$STYPE','IN PROGRESS')"
+      sqlite3 $WORKDIR/sessions.sqlite3 "insert into backup_session(sessionID,initial_date,type,status) values ('$SESSION','$DATE','$STYPE','IN PROGRESS')" > /dev/null 2>&1
     fi
     if [[ "$SESSION" == "full"* ]] || [[ "$SESSION" == "inc"* ]]; then
       cat $TEMPACCOUNT | parallel --no-notice --jobs $MAX_PARALLEL_PROCESS \
@@ -120,8 +120,8 @@ function backup_main()
     elif [[ $SESSION_TYPE == "SQLITE3" ]]; then
       DATE=$(date +%Y-%m-%dT%H:%M:%S.%N)
       SIZE=$(du -sh $WORKDIR/$i | awk {'print $1'})
-      sqlite3 $WORKDIR/sessions.sqlite3 < $TEMPSQL
-      sqlite3 $WORKDIR/sessions.sqlite3 "update backup_session set conclusion_date='$DATE',size='$SIZE',status='FINISHED' where sessionID='$SESSION'"
+      sqlite3 $WORKDIR/sessions.sqlite3 < $TEMPSQL > /dev/null 2>&1
+      sqlite3 $WORKDIR/sessions.sqlite3 "update backup_session set conclusion_date='$DATE',size='$SIZE',status='FINISHED' where sessionID='$SESSION'" > /dev/null 2>&1
     fi
     logger -i -p local7.info "Zmbackup: Backup session $SESSION finished on $(date)"
     echo "Backup session $SESSION finished on $(date)"
