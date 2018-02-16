1e402143729ac719077c8f77e078af2510e84454
Bruno <bruno@imotion-info.com>
2018-02-16 16:42:10 -0200

    Fixed bug that make notify mail don't print correct backup status

--- project/lib/bash/MiscAction.sh
+++ project/lib/bash/MiscAction.sh
@@ -8,10 +8,10 @@
 ################################################################################
 function on_exit(){
   ERRCODE=$?
-  if [[ $ERRCODE -eq 1 ]]; then
-    notify_finish $SESSION $STYPE "FAILURE"
+  if [[ $ERRCODE > 0 ]]; then
+    notify_finish "$SESSION" "$STYPE" "FAILURE"
   elif [[ $ERRCODE -eq 0 && ! -z $SESSION ]]; then
-    notify_finish $SESSION $STYPE "SUCCESS"
+    notify_finish "$SESSION" "$STYPE" "SUCCESS"
   fi
   rm -rf $TEMPSESSION $TEMPACCOUNT $TEMPINCACCOUNT $TEMPDIR $MESSAGE $TEMPSQL $FAILURE
   logger -i -p local7.info "Zmbackup: Excluding the temporary files before close."
