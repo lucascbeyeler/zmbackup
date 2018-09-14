84914bc2e84f7a526d99e825e29bd74606d8d038
cavassin <cavassin@gmail.com>
2018-09-14 11:07:10 -0300

    Update ParallelAction.sh
    Enhanced existence test for TXT session, so EXIST variable will have same semantics of SQLITE3 session, i.e., non empty variable means backup already done.
    Fixed existence test for SQLITE3 session, looking for specific email, not for sessionID.
    Simplified following test.

--- project/lib/bash/ParallelAction.sh
+++ project/lib/bash/ParallelAction.sh
@@ -108,16 +108,16 @@ function mailbox_restore()
 function ldap_filter()
 {
   if [[ "$SESSION_TYPE" == "TXT" ]]; then
-    EXIST=$(grep $1 $WORKDIR/sessions.txt 2> /dev/null | tail -1 | awk -F: '{print $3}')
+    EXIST=$(grep "$1:$(date +%m/%d/%y)" $WORKDIR/sessions.txt 2> /dev/null | tail -1)
   else
     TODAY=$(date +%Y-%m-%dT%H:%M:%S.%N)
     YESTERDAY=$(date +%Y-%m-%dT%H:%M:%S.%N -d "yesterday")
-    EXIST=$(sqlite3 $WORKDIR/sessions.sqlite3 "select email from backup_account where conclusion_date < '$TODAY' and conclusion_date > '$YESTERDAY' and sessionID='$1'")
+    EXIST=$(sqlite3 $WORKDIR/sessions.sqlite3 "select email from backup_account where conclusion_date < '$TODAY' and conclusion_date > '$YESTERDAY' and email='$1'")
   fi
   grep -Fxq $1 /etc/zmbackup/blacklist.conf
   if [[ $? -eq 0 ]]; then
     echo "WARN: $1 found inside blacklist - Nothing to do."
-  elif [[ -n $EXIST && "$EXIST" = "$(date +%m/%d/%y)" ]] && [[ "$LOCK_BACKUP" == "true" ]]; then
+  elif [[ $EXIST ]] && [[ "$LOCK_BACKUP" == "true" ]]; then
     echo "WARN: $1 already has backup today. Nothing to do."
   else
     echo $1 >> $TEMPACCOUNT
